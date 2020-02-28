584a72ad7f7324dbc49d438601ce2aa90834f167
beahaus <54328805+beahaus@users.noreply.github.com>
2020-02-28 03:32:38 -0500

    creation of layout, cards, javascript variable declarations, and a functions.js page

---
+++ variableDeclaration.js
@@ -0,0 +1,30 @@
+
+var beginningCard = document.querySelector("card.mx-auto.mt-5.mb-5.beginningCar");
+
+var startButton = document.querySelector("btn.btn-primary.mt-1.start");
+
+var questionNumber = "";
+
+var question1content = "Question 2 Content";
+var question1answers = ["correct", "wrong 1", "wrong 2", "wrong 3"];
+
+var question2content = "Question 2 Content";
+var question2answers = ["correct", "wrong 1", "wrong 2", "wrong 3"];
+
+var question3content = "Question 3 Content";
+var question3answers = ["correct", "wrong 1", "wrong 2", "wrong 3"];
+
+var question4content = "Question 4 Content";
+var question4answers = ["correct", "wrong 1", "wrong 2", "wrong 3"];
+
+var option1 = document.querySelector("btn.btn-primary.mt-1.option1");
+
+var option2 = document.querySelector("btn.btn-primary.option2");
+
+var option3 = document.querySelector("btn.btn-primary.option3");
+
+var option4 = document.querySelector("btn.btn-primary.option4");
+
+var score = "";
+
+
