4fcf567f2362e251ee10c80aeb0c5c118de2b7d2
beahaus <54328805+beahaus@users.noreply.github.com>
2020-03-05 15:16:11 -0500

    changed question contents to an object, added variable declarations to functions.js and deleted separate variable declaration js page

--- functions.js
+++ functions.js
@@ -1,10 +1,70 @@
+// variable declarations
+var beginningCard = document.getElementById("beginning-card");
+
+var startButton = document.getElementById("start-button");
+
+var timer = document.getElementsByClassName("timer");
+var timerText = document.getElementById("timer-text");
+var minutesDisplay = document.querySelector("#minutes");
+var secondsDisplay = document.querySelector("#seconds");
+
+var quizCard = document.getElementById("quiz-card");
+var questionNumber = document.getElementById("question-number");
+var questionContent = document.getElementById("question-content");
+
+var option1 = document.getElementById("option1");
+var option2 = document.getElementById("option2");
+var option3 = document.getElementById("option3");
+var option4 = document.getElementById("option4");
+
+// question contents
+var questionContents = { 
+    question1: {
+        content: "Question 1 Content",
+        correctAnswer: "correct",
+        wrongAnswers: ["wrong1", "wrong2", "wrong3"]
+    },
+    question2: {
+        content: "Question 2 Content",
+        correctAnswer: "correct",
+        wrongAnswers: ["wrong1", "wrong2", "wrong3"]
+    },
+    question3: {
+        content: "Question 3 Content",
+        correctAnswer: "correct",
+        wrongAnswers: ["wrong1", "wrong2", "wrong3"]
+    },
+    question4: {
+        content: "Question 4 Content",
+        correctAnswer: "correct",
+        wrongAnswers: ["wrong1", "wrong2", "wrong3"]
+    },
+    question5: {
+        content: "Question 5 Content",
+        correctAnswer: "correct",
+        wrongAnswers: ["wrong1", "wrong2", "wrong3"]
+    },
+
+};
+// question contents 
+
+var scoreTotal = document.getElementById("score-total");
+
+var endingCard = document.getElementById("ending-card");
+var scoreCard = document.getElementById("score-card");
+var highScoresList = document.getElementsByClassName("high-scores-list");
+var recordButton = document.getElementById("record-button");
+var restartButton = document.getElementById("restart-button");
+
 
 var timer;
 var score = 0;
 var totalSeconds = 180;
 var secondsElapsed = 0;
-var initialsInput = [];
-var finalScores = [];
+var intials = document.querySelector(".initials");
+// var initialsInput = []; 
+// initialsInput[0] = localStorage.setItem("initials", JSON.stringify(initials));
+// variable declarations
 
 // timer functions
 function getFormattedMinutes() { 
@@ -33,7 +93,7 @@ function renderTime() {
 function stopTimer() {
     secondsElapsed = 0;
     renderTime(); 
-    answeredQ5();
+    // answeredQ5();
   }
 
 function startTimer() {
@@ -63,11 +123,11 @@ function startQuiz () {
     beginningCard.style.display = "none";
     quizCard.style.display = "block";
     questionNumber.textContent = "Question 1 of 5";
-    questionContent.textContent = question1content;
-    option1.textContent = question1answers[1];
-    option2.textContent = question1answers[3];
-    option3.textContent = question1answers[0];
-    option4.textContent = question1answers[2];
+    questionContent.textContent = questionContents.question1.content;
+    option1.textContent = questionContents.question1.wrongAnswers[1];
+    option2.textContent = questionContents.question1.wrongAnswers[2];
+    option3.textContent = questionContents.question1.correctAnswer;
+    option4.textContent = questionContents.question1.wrongAnswers[0];
 
     option3.onclick = function () {
         score += 20;
@@ -92,11 +152,11 @@ function startQuiz () {
 function answeredQ1 () {
     renderTime();
     questionNumber.textContent = "Question 2 of 5";
-    questionContent.textContent = question2content;
-    option1.textContent = question2answers[0];
-    option2.textContent = question2answers[3];
-    option3.textContent = question2answers[2];
-    option4.textContent = question2answers[1];
+    questionContent.textContent = questionContents.question2.content;
+    option1.textContent = questionContents.question2.correctAnswer;
+    option2.textContent = questionContents.question2.wrongAnswers[2];
+    option3.textContent = questionContents.question2.wrongAnswers[1];
+    option4.textContent = questionContents.question2.wrongAnswers[0];
 
     option1.onclick = function () {
         score += 20;
@@ -121,11 +181,11 @@ function answeredQ1 () {
 // show question 3 content after question 2 is answered
 function answeredQ2 () {
     questionNumber.textContent = "Question 3 of 5";
-    questionContent.textContent = question3content;
-    option1.textContent = question3answers[3];
-    option2.textContent = question3answers[1];
-    option3.textContent = question3answers[0];
-    option4.textContent = question3answers[2];
+    questionContent.textContent = questionContents.question3.content;
+    option1.textContent = questionContents.question3.wrongAnswers[1];
+    option2.textContent = questionContents.question3.wrongAnswers[2];
+    option3.textContent = questionContents.question3.correctAnswer;
+    option4.textContent = questionContents.question3.wrongAnswers[0];
 
     option3.onclick = function () {
         score += 20;
@@ -149,11 +209,11 @@ function answeredQ2 () {
 // show question 4 content after question 3 is answered
 function answeredQ3 () {
     questionNumber.textContent = "Question 4 of 5";
-    questionContent.textContent = question4content;
-    option1.textContent = question4answers[1];
-    option2.textContent = question4answers[2];
-    option3.textContent = question4answers[0];
-    option4.textContent = question4answers[3];
+    questionContent.textContent = questionContents.question4.content;
+    option1.textContent = questionContents.question4.wrongAnswers[1];
+    option2.textContent = questionContents.question4.wrongAnswers[2];
+    option3.textContent = questionContents.question4.correctAnswer;
+    option4.textContent = questionContents.question4.wrongAnswers[0];
     
     option3.onclick = function () {
         score += 20;
@@ -177,11 +237,11 @@ function answeredQ3 () {
 // show question 5 content after question 4 is answered
 function answeredQ4 () {
     questionNumber.textContent = "Question 5 of 5";
-    questionContent.textContent = question5content;
-    option1.textContent = question5answers[3];
-    option2.textContent = question5answers[2];
-    option3.textContent = question5answers[1];
-    option4.textContent = question5answers[0];
+    questionContent.textContent = questionContents.question5.content;
+    option1.textContent = questionContents.question5.wrongAnswers[0];
+    option2.textContent = questionContents.question5.wrongAnswers[2];
+    option3.textContent = questionContents.question5.wrongAnswers[1];
+    option4.textContent = questionContents.question5.correctAnswer;
    
     option4.onclick = function () {
         score += 20;
@@ -201,36 +261,34 @@ function answeredQ4 () {
 
 // store initials and score in localStorage and stringify
 function storeInitials() {
-    localStorage.setItem("initials", JSON.stringify(initialsInput));
+    localStorage.setItem("initials", initials);
 }
 function storeScore() {
-    localStorage.setItem("score", JSON.stringify(finalScores));
+    localStorage.setItem("score", score);
 }
 // store initials and score in localStorage and stringify
 
-// get stored initials and score from localStorage and parse into object
+// get stored initials and score from localStorage
 
-// get stored initials and score from localStorage and parse into object
+// get stored initials and score from localStorage
 
 // show card for recording intials and show score after question 5 is answered
 function answeredQ5 () {
     quizCard.style.display = "none";
     endingCard.style.display = "block";
     scoreTotal.textContent = "Your final score is " + score + " out of 100";
+    finalScore = score;
     stopTimer();
-    
-    // call functions to store initials and score
-    storeInitials();
-    storeScore();
 }
 // show card for recording initals and show score after question 5 is answered
 
 // show recorded high scores
-recordButton.onclick = function (event) { 
+recordButton.onclick = function () { 
     endingCard.style.display = "none";
     scoreCard.style.display = "block";
     // render initials and scores in ul
-
+    //localStorage.setItem("initials", initials);
+    //localStorage.setItem("score", score);
     }
 // show recorded high scores
 
