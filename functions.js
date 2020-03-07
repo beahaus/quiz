c89dad9fbd0f1db33f4b6b18e7c2c7db12a28410
beahaus <54328805+beahaus@users.noreply.github.com>
2020-03-07 09:17:46 -0500

    updted question contents

--- functions.js
+++ functions.js
@@ -24,29 +24,29 @@ var option4 = document.getElementById("option4");
 // question contents
 var questionContents = { 
     question1: {
-        content: "Question 1 Content",
-        correctAnswer: "correct",
-        wrongAnswers: ["wrong1", "wrong2", "wrong3"]
+        content: "Which of the following tags do you use to insert Javascript into an HTML page?",
+        correctAnswer: "<script>",
+        wrongAnswers: ["<style>", "<javascript>", "<script=javascript>"]
     },
     question2: {
-        content: "Question 2 Content",
-        correctAnswer: "correct",
-        wrongAnswers: ["wrong1", "wrong2", "wrong3"]
+        content: "What language defines the behavior of a web page?",
+        correctAnswer: "Javascript",
+        wrongAnswers: ["HTML", "CSS", "XHTML"]
     },
     question3: {
-        content: "Question 3 Content",
-        correctAnswer: "correct",
-        wrongAnswers: ["wrong1", "wrong2", "wrong3"]
+        content: "Does Javascript ignore extra spaces?",
+        correctAnswer: "True",
+        wrongAnswers: ["False"]
     },
     question4: {
-        content: "Question 4 Content",
-        correctAnswer: "correct",
-        wrongAnswers: ["wrong1", "wrong2", "wrong3"]
+        content: "Which of the below is used to insert special characters into Javascript?",
+        correctAnswer: "\\",
+        wrongAnswers: ["&", "$", "!"]
     },
     question5: {
-        content: "Question 5 Content",
-        correctAnswer: "correct",
-        wrongAnswers: ["wrong1", "wrong2", "wrong3"]
+        content: "Is Javascript and Object Oriented Language?",
+        correctAnswer: "True",
+        wrongAnswers: ["False"]
     },
 
 };
@@ -189,23 +189,15 @@ function answeredQ1 () {
 // show question 3 content after question 2 is answered
 function answeredQ2 () {
     questionNumber.textContent = "Question 3 of 5";
+    option1.style.display = "none";
+    option2.style.display = "none";
     questionContent.textContent = questionContents.question3.content;
-    option1.textContent = questionContents.question3.wrongAnswers[1];
-    option2.textContent = questionContents.question3.wrongAnswers[2];
     option3.textContent = questionContents.question3.correctAnswer;
     option4.textContent = questionContents.question3.wrongAnswers[0];
 
     option3.onclick = function () {
         score += 20;
         answeredQ3();
-    }
-    option1.onclick = function () {
-        secondsElapsed= secondsElapsed+30;
-        answeredQ3();
-    }
-    option2.onclick = function () {
-        secondsElapsed= secondsElapsed+30;
-        answeredQ3(); 
     }
     option4.onclick = function () {
         secondsElapsed= secondsElapsed+30;
@@ -217,6 +209,8 @@ function answeredQ2 () {
 // show question 4 content after question 3 is answered
 function answeredQ3 () {
     questionNumber.textContent = "Question 4 of 5";
+    option1.style.display = "block";
+    option2.style.display = "block";
     questionContent.textContent = questionContents.question4.content;
     option1.textContent = questionContents.question4.wrongAnswers[1];
     option2.textContent = questionContents.question4.wrongAnswers[2];
@@ -245,23 +239,17 @@ function answeredQ3 () {
 // show question 5 content after question 4 is answered
 function answeredQ4 () {
     questionNumber.textContent = "Question 5 of 5";
+    option3.style.display = "none";
+    option4.style.display = "none";
     questionContent.textContent = questionContents.question5.content;
-    option1.textContent = questionContents.question5.wrongAnswers[0];
-    option2.textContent = questionContents.question5.wrongAnswers[2];
-    option3.textContent = questionContents.question5.wrongAnswers[1];
-    option4.textContent = questionContents.question5.correctAnswer;
+    option1.textContent = questionContents.question5.correctAnswer;
+    option2.textContent = questionContents.question5.wrongAnswers[0];
    
-    option4.onclick = function () {
+    option1.onclick = function () {
         score += 20;
         answeredQ5();
     }
-    option1.onclick = function () {
-        answeredQ5();
-    }
     option2.onclick = function () {
-        answeredQ5(); 
-    }
-    option3.onclick = function () {
         answeredQ5();
     }
 }
@@ -269,6 +257,8 @@ function answeredQ4 () {
 
 // show card for recording intials and show score after question 5 is answered
 function answeredQ5 () {
+    option3.style.display = "block";
+    option4.style.display = "block";
     quizCard.style.display = "none";
     endingCard.style.display = "block";
     scoreTotal.textContent = "Your final score is " + score + " out of 100";
@@ -301,7 +291,7 @@ recordButton.onclick = function() {
     function scoresLists() {
         var li = document.createElement("li");
 
-        var highInitialsList = document.getElementById("high-initials-list");
+        var highInitialsList = document.getElementById("high-scores-list");
         var x;
         for (x = 0; x < initialsInput.length; x++) {  
             li.append(initialsInput[x]);
